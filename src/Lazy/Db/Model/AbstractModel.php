57b3f4a1fc4277103f6f64c50e50467d698f8f08
lytc <prtran@gmail.com>
2013-05-08 13:55:40 +0700

    fix association bug

--- src/Lazy/Db/Model/AbstractModel.php
+++ src/Lazy/Db/Model/AbstractModel.php
@@ -697,8 +697,10 @@ abstract class AbstractModel
                     $associationData[$row[$leftKey]][] = $row;
                 }
 
-                foreach ($associationData as $id => $rows) {
-                    $collectionClass = $refModel::collectionClass();
+                $collectionClass = $refModel::collectionClass();
+
+                foreach ($collectionIds as $id) {
+                    $rows = isset($associationData[$id])? $associationData[$id] : array();
                     $this->collection->get($id)->set($name, new $collectionClass($rows));
                 }
 
